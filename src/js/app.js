d0373122f3ad3a68f546f754ec724126950ab5de
Ramiz Ashurov <dima-sedov-1996@mail.ru>
2024-03-21 15:23:03 +0300

    about page fixs

--- src/js/app.js
+++ src/js/app.js
@@ -20,18 +20,6 @@ window.onload = function() {
         }
     }; 
 
-    // device detection
-    // if (
-    //     /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent) 
-    //     || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0,4))
-    //    ) { 
-    //     isMobile = true;
-    // }
-
-    // function validateEmail(email) {
-    //     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    //     return re.test(String(email).toLowerCase());
-    // }
 
     function lockBody() {
         // let paddingValue = window.innerWidth - document.documentElement.clientWidth;
@@ -149,50 +137,7 @@ window.onload = function() {
     const headerCityEl = headerEl.querySelector(".header__city");
     const burgerMenuEl = headerEl.querySelector(".header__burger");
 
-    const citiesItemEls = document.querySelectorAll(".popup .city")
-
-    // Popups
-    const citiesPopupEl = document.querySelector(".popup_cities");
-    const callBackPopupEl = document.querySelector(".popup_callback")
-
-    headerCityEl.addEventListener("click", () => {
-        openPopup(citiesPopupEl)
-    })
-
-    citiesItemEls.forEach(cityItemEl => cityItemEl.addEventListener("click", () => {
-        changeCity(
-            cityItemEl.dataset.city,
-            cityItemEl.lastElementChild.innerHTML
-        )
-        closePopup()
-        document.querySelector(".city_active").classList.remove("city_active")
-        cityItemEl.classList.add("city_active")
-    }))
-
-    document
-        .querySelectorAll(".header__call-desc, .header__button, .header__actions .header__call-icon, .hero-section__button, .footer__button")
-        .forEach(button => button.addEventListener("click", () => openPopup(callBackPopupEl)))
-
-    document.querySelectorAll(".popup__close").forEach(
-        closeEl => closeEl.addEventListener("click", closePopup)
-    )
-
-    document.querySelectorAll(".popup").forEach(
-        popupEl => popupEl.addEventListener("click", e => {
-            if (!e.target.closest(".popup__content")) {
-                closePopup()
-            }
-        })
-    )
-
-    document.addEventListener("keydown", (e) => {
-        if (e.which === 27) {
-            closePopup()
-        }
-    })
-
     const menuEl = document.querySelector(".header__menu")
-    const menuItemEls = menuEl.querySelectorAll(".header__menu-item");
 
     menuEl.addEventListener("click", e => {
         const targetEl = e.target;
@@ -207,7 +152,6 @@ window.onload = function() {
 
         e.preventDefault();
         menuItemEl.classList.toggle("header__menu-item_open")
-
     })
 
     const tabletMediaQuery = window.matchMedia("(max-width: 1080px)") 
@@ -268,7 +212,6 @@ window.onload = function() {
     redrawMenu(phoneMediaQuery)
     replaceFooterMenu(tabletMediaQuery)
     
-    
     burgerMenuEl.addEventListener("click", () => {
         if (burgerMenuEl.classList.contains("header__burger_open")) {
             document.querySelectorAll(".header__menu-item_open")
@@ -296,7 +239,49 @@ window.onload = function() {
         unlockBody()
     })
 
-    // form 
+    // Popups
+    const citiesItemEls = document.querySelectorAll(".popup .city")
+    const citiesPopupEl = document.querySelector(".popup_cities");
+    const callBackPopupEl = document.querySelector(".popup_callback")
+
+    headerCityEl.addEventListener("click", () => {
+        openPopup(citiesPopupEl)
+    })
+
+    citiesItemEls.forEach(cityItemEl => cityItemEl.addEventListener("click", () => {
+        changeCity(
+            cityItemEl.dataset.city,
+            cityItemEl.lastElementChild.innerHTML
+        )
+        closePopup()
+        document.querySelector(".city_active").classList.remove("city_active")
+        cityItemEl.classList.add("city_active")
+    }))
+
+    document
+        .querySelectorAll(".header__call-desc, .header__button, .header__actions .header__call-icon, .hero-section__button, .footer__button")
+        .forEach(button => button.addEventListener("click", () => openPopup(callBackPopupEl)))
+
+    document.querySelectorAll(".popup__close").forEach(
+        closeEl => closeEl.addEventListener("click", closePopup)
+    )
+
+    document.querySelectorAll(".popup").forEach(
+        popupEl => popupEl.addEventListener("click", e => {
+            if (!e.target.closest(".popup__content")) {
+                closePopup()
+            }
+        })
+    )
+
+    document.addEventListener("keydown", (e) => {
+        if (e.which === 27) {
+            closePopup()
+        }
+    })
+
+
+    // Form 
     const inputEls = document.querySelectorAll(".form__input, .form__check-input")
     const inputControlClass = "form__control"
 
@@ -458,11 +443,16 @@ new Swiper(".projects-section__swiper", {
 // reviews & seo text
 const reviewItemEls = document.querySelectorAll(".review");
 const seoContentEl = document.querySelector(".seo-content")
+const maxHeights = {};
 
-const maxHeights = {
-    review: parseFloat(getComputedStyle(document.querySelector(".review__text")).maxHeight),
-    seo: parseFloat(getComputedStyle(document.querySelector(".seo-content__text")).maxHeight),
+if (reviewItemEls) {
+    maxHeights.review = parseFloat(getComputedStyle(document.querySelector(".review__text")).maxHeight)
 }
+
+if (seoContentEl) {
+    maxHeights.seo = parseFloat(getComputedStyle(document.querySelector(".seo-content__text")).maxHeight)
+}
+
 
 function changeElemHeight(elem) {
     const buttonTextEl = elem.querySelector("span")
@@ -498,14 +488,17 @@ reviewItemEls.forEach(reviewItemEl => {
     reviewItemEl.querySelector(".review__more").addEventListener("click", () => changeElemHeight(reviewItemEl))
 })
 
-checkElemHeight(seoContentEl, maxHeights.seo);
-seoContentEl.querySelector(".seo-content__more").addEventListener("click", () => changeElemHeight(seoContentEl))
 
 if (reviewItemEls.length) {
     window.addEventListener("resize", () => {
         reviewItemEls.forEach(reviewItemEl => checkElemHeight(reviewItemEl, maxHeights.review))
-        checkElemHeight(seoContentEl, maxHeights.seo)
     })
+}
+
+if (seoContentEl) {
+    checkElemHeight(seoContentEl, maxHeights.seo);
+    seoContentEl.querySelector(".seo-content__more").addEventListener("click", () => changeElemHeight(seoContentEl))
+    window.addEventListener("resize", () => checkElemHeight(seoContentEl, maxHeights.seo))
 }
 
 // yandex map
