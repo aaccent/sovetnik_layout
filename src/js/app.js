c5eb45a73e3a94ffef55dfa1d908022bc64c7cd2
Sevastyan <seva@aaccent.ru>
2025-04-11 12:45:06 +0300

    fix: issue with form__file-input

--- src/js/app.js
+++ src/js/app.js
@@ -358,7 +358,7 @@ window.onload = function () {
         })
     })
 
-    document.querySelector(".form__file-input").addEventListener("change", e => {
+    document.querySelector(".form__file-input")?.addEventListener("change", e => {
         if (e.target.files[0].size > 100 * 1024 * 1024) {
             alert("Размер файла не должен превышать 30 MB")
             return
